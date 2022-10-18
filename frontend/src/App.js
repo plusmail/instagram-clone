1c974721f2668b1cb370c2fe9d17e321d693f8a0
hyesu2 <zpswis09@naver.com>
2022-10-18 12:07:30 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-
+import Main from "./Main/Main";
 
 function App() {
-  return (
-      <div></div>
-  )
+    return (
+        <div>
+            {/*여기에 네비바 넣으시면 됩니다*/}
+            <Main/>
+        </div>
+    );
 }
 
 export default App;
