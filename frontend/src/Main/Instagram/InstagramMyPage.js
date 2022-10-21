6d7082562a93eb48f1afc2cb04966496f66ca35f
jellikim <jellikiim@gmail.com>
2022-10-21 11:17:56 +0900

    auth done

--- frontend/src/Main/Instagram/InstagramMyPage.js
+++ frontend/src/Main/Instagram/InstagramMyPage.js
@@ -1,15 +1,15 @@
-import HeaderContainer from '../../Authority/containers/common/HeaderContainer';
-import MyPageContainer from "../../MyPage/MyPageContainer";
+import Header from '../../Authority/components/common/Header';
+import MyPageContainer from '../../MyPage/MyPageContainer';
 
 function InstagramMyPage() {
-    return (
-        <div className={'all'}>
-            {/*임시 로그아웃 버튼*/}
-            <HeaderContainer />
-            <div className={'fixed'}>{/*네비바 여기 오면 됩니다.*/}</div>
-            <MyPageContainer/>
-        </div>
-    );
+  return (
+    <div className={'all'}>
+      {/*임시 로그아웃 버튼*/}
+      <Header />
+      <div className={'fixed'}>{/*네비바 여기 오면 됩니다.*/}</div>
+      <MyPageContainer />
+    </div>
+  );
 }
 
 export default InstagramMyPage;
--- frontend/src/Main/Instagram/InstagramPeople.js
+++ frontend/src/Main/Instagram/InstagramPeople.js
@@ -1,15 +1,15 @@
-import HeaderContainer from '../../Authority/containers/common/HeaderContainer';
-import People from "../../Suggest/People";
+import Header from '../../Authority/components/common/Header';
+import People from '../../Suggest/People';
 
 function InstagramPeople() {
-    return (
-        <div className={'all'}>
-            {/*임시 로그아웃 버튼*/}
-            <HeaderContainer />
-            <div className={'fixed'}>{/*네비바 여기 오면 됩니다.*/}</div>
-            <People/>
-        </div>
-    );
+  return (
+    <div className={'all'}>
+      {/*임시 로그아웃 버튼*/}
+      <Header />
+      <div className={'fixed'}>{/*네비바 여기 오면 됩니다.*/}</div>
+      <People />
+    </div>
+  );
 }
 
 export default InstagramPeople;
