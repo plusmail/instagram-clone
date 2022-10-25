ad21ec836f650b92b92e2c51be3dfa073aeb247f
edu03 <  >
2022-10-25 16:45:42 +0900

    'Navbar css 수정'

--- frontend/src/NavBar/NavBar.js
+++ frontend/src/NavBar/NavBar.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import './NavBar.css';
-import Grid from '@material-ui/core/Grid';
 import Search from "./Search/Search";
 import img from "./img/insta_logo.png";
 import Modal from "./Modal/Modal";
@@ -15,30 +14,21 @@ class NavBar extends Component {
     render() {
         return (
             <div className="navbar__barContent">
-                <div className='container'>
-                    <Grid item xs={1}>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Link to={'instagram'}>
-                            <img className='navbar_logo' src={img} alt='logo' width='100px'  />
-                        </Link>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <div className='Search-container'>
-                            <Search/>
-                        </div>
-                    </Grid>
-                    <Grid item xs={4}>
+                <div className='logo-container'>
+                    <Link to={'https://localhost:3000/'}>
+                        <img className='navbar_logo' src={img} alt='logo' />
+                    </Link>
+                </div>
+                    <div className='Search-container'>
+                        <Search/>
+                    </div>
                     <div className='modal-container'>
                         <Modal/>
                     </div>
-                    </Grid>
-                </div>
             </div>
         )
     }
 }
-
 
 
 export default NavBar;
